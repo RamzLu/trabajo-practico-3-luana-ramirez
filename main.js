4d21d715cf96a7a25a51c036e86d417f69682398
Luana Ramirez <luanaabigail168@gmail.com>
2025-05-29 23:15:47 -0300

    se realizó las funciones de pedir la api y otra de mostrar los personajes

---
+++ main.js
@@ -0,0 +1,72 @@
+const contenedorPrincipal = document.getElementById("contenedor-data")
+const apiDragonBall = "https://dragonball-api.com/api/characters?limit=30";
+
+// Pide los datos a la api del internet y la retorna en "data"
+async function cargarDatosApi(url) {
+    try {
+        const response = await fetch(url); // busca la api
+    
+        if (!response.ok) {
+            throw new Error("Error API");
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+// funcion para mostrar los personajes
+function mostrarPersonajesDB(personajes) {
+    personajes.forEach(function (personaje) {
+        contenedorPrincipal.innerHTML += `
+        <div class="col-3 pb-2 d-flex justify-content-center" data-id="${personaje.id}">
+            <div class="card" style="width: 15rem;">
+                <img class="card-img-top" src="${personaje.image}" />
+                <div class="card-body">
+                   <h5 class="card-title">${personaje.name}</h5>
+                    <p class="card-text">${personaje.race} - ${personaje.gender}</p>
+                    <button class="btn btn-success btn-ver-detalles">Ver más</button>
+                </div>
+            </div>
+        </div>    
+    `;
+    });
+}
+
+function activarEventoVerDetalles() {
+  contenedorPrincipal.addEventListener("click", function(e) {
+    if (e.target.classList.contains("btn-ver-detalles")) {
+      const cardPadre = e.target.closest(".col-3");
+      const id = cardPadre.dataset.id;
+      verDetalles(id);
+    }
+  });
+}
+activarEventoVerDetalles();
+
+
+async function verDetalles(id) {
+  try {
+    const response = await fetch(`https://dragonball-api.com/api/characters/${id}`);
+
+    if (!response.ok) {
+      throw new Error("Error API");
+    }
+
+    const data = await response.json();
+    alert(data.description);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+window.addEventListener("DOMContentLoaded", iniciarPagina);
+
+async function iniciarPagina() {
+    const data = await cargarDatosApi(apiDragonBall);
+    if (data && data.items) {
+        mostrarPersonajesDB(data.items);
+    }
+}
